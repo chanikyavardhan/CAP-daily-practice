780a95cb8c6a7aa4afe5358bea725cbf23a801c4
chanikyavardhan <85987029+chanikyavardhan@users.noreply.github.com>
2023-01-29 22:29:18 +0530

    Create all elements in two binary search trees.java

---
+++ 29-1-2023/all elements in two binary search trees.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list1 = new ArrayList<>();
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        inorder(root1,list1);
+        inorder(root2,list1);
+        Collections.sort(list1);
+        return list1;
+    }
+    public void inorder(TreeNode root,List<Integer> list){
+        if(root==null){
+            return;
+        }
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
+    }
+}
