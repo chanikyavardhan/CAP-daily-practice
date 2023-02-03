d374bca38e5a346f079a8895f7b5485f69dd6328
chanikyavardhan <85987029+chanikyavardhan@users.noreply.github.com>
2023-02-03 21:16:43 +0530

    Create Two sum-IV BST.java

---
+++ 3-2-2023/Two sum-IV BST.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Set<Integer> set = new HashSet<>();
+    public boolean findTarget(TreeNode root, int k) {
+        return solve(root,k);
+    }
+    public boolean solve(TreeNode root,int k){
+        if(root==null){
+            return false;
+        }
+        if(solve(root.left,k)) return true;
+        if(set.contains(k-root.val)){
+            return true;
+        }
+        set.add(root.val);
+        return solve(root.right,k);
+
+    }
+}
