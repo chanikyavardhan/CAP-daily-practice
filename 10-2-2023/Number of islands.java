c4b4a043ee0240ee5d2d1ab70042fb516ce321b2
chanikyavardhan <85987029+chanikyavardhan@users.noreply.github.com>
2023-02-11 10:33:01 +0530

    Create Number of islands.java

---
+++ 10-2-2023/Number of islands.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    countIslands(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+    public void countIslands(char[][] grid,int i,int j){
+        if(i<0||j<0||i>=grid.length||j>=grid[i].length||grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j] = '0';
+        countIslands(grid,i+1,j);
+        countIslands(grid,i-1,j);
+        countIslands(grid,i,j+1);
+        countIslands(grid,i,j-1);
+    }
+}
