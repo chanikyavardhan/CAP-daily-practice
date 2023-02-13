2051ba9c4c71dba2f4836300c50e5d0da7211b64
chanikyavardhan <85987029+chanikyavardhan@users.noreply.github.com>
2023-02-13 22:09:32 +0530

    Create Minimum number of vertices to reach all nodes.java

---
+++ 13-2-2023/Minimum number of vertices to reach all nodes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        List<Integer> res = new ArrayList();
+        boolean[] inDegree = new boolean[n];
+        for (List<Integer> edge : edges) {
+            inDegree[edge.get(1)] = true;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!inDegree[i])
+                res.add(i);
+        }
+        return res;
+    }
+}
